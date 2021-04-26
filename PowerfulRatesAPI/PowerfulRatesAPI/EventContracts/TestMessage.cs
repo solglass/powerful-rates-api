307a14a43dca70081efaf458a5948da6c3de08ef
SimonShvets <goodweather.spb@gmail.com>
2021-04-26 21:11:25 +0300

    Start project

---
+++ PowerfulRatesAPI/PowerfulRatesAPI/EventContracts/TestMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventContracts
+{
+    public class TestMessage
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ PowerfulRatesAPI/PowerfulRatesAPI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using EventContracts;
+using MassTransit;
+
+namespace PowerfulRatesAPI
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg => cfg.Host("80.78.240.16", hst => 
+            {
+                hst.Username("volodya22");
+                hst.Password("qwe!@#");
+            }));
+
+            await busControl.StartAsync();
+            try
+            {
+                while (true)
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter message (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    await busControl.Publish<TestMessage>(new
+                    {
+                        Value = value
+                    });
+                }
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
