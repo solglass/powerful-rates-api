e839fe8f76ad12bfc88290421375c4c86dc5ba21
Denis Valeev <valeevden@gmail.com>
2021-04-29 22:02:23 +0300

    External CurrencyRates add

--- PowerfulRatesAPI/PowerfulRatesWebAPI/Controllers/ValueController.cs
+++ PowerfulRatesAPI/PowerfulRatesWebAPI/Controllers/ValueController.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 namespace PowerfulRatesWebAPI
 {
     using System;
+    using System.Text.Json;
     using System.Threading.Tasks;
     using EventContracts;
     using MassTransit;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
     [Route("api/[controller]")]
 
-    public class ValueController :
-        ControllerBase
+    public class ValueController : ControllerBase
     {
         readonly IPublishEndpoint _publishEndpoint;
 
@@ -33,6 +34,22 @@ namespace PowerfulRatesWebAPI
             });
 
             return Ok();
+        }
+
+        /// <summary>
+        /// Get json CurrencyRates
+        /// </summary>
+        /// <returns>Get json CurrencyRates</returns>
+        // https://localhost:44365/api/transaction/42
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [HttpGet("rates")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<string> GetRates()
+        {
+            //var result = new CurrencyRates().GetCurrencyRates();
+            var result = new CurrencyRates();
+
+            return Ok(result.GetCurrencyRates());
         }
     }
 }
---
+++ PowerfulRatesAPI/PowerfulRatesWebAPI/CurrencyRate.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace PowerfulRatesWebAPI
+{
+    public class CurrencyRates
+    {
+        public const string url = "http://api.currencylayer.com/live?access_key=e4f2e8790e61ca39430fd98df8bd299b";
+        public RestClient _client = new RestClient(url);
+        public RestRequest _request = new RestRequest(Method.GET);
+
+        //public JsonDocument response;
+
+        public string GetCurrencyRates()
+        {
+            var response = _client.Execute<string>(_request);
+            return response.Data;
+        }
+       
+    }
+}
