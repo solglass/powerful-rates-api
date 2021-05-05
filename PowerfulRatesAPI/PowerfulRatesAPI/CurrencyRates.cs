e48c6e41d040ae38cca73bfea729381ae30a17ff
SimonShvets <goodweather.spb@gmail.com>
2021-05-05 17:51:27 +0300

    add configuration

--- PowerfulRatesAPI/PowerfulRatesAPI/CurrencyRates.cs
+++ PowerfulRatesAPI/PowerfulRatesAPI/CurrencyRates.cs
@@ -10,7 +10,7 @@ namespace PowerfulRatesAPI
 {
     public static class CurrencyRates
     {
-        public const string url = "http://api.currencylayer.com/live?access_key=e4f2e8790e61ca39430fd98df8bd299b";
+        public static string url = Program.Configuration.GetSection("CurrencyRatesSourse").Value;
         public static RestClient _client = new RestClient(url);
         public static RestRequest _request = new RestRequest(Method.GET);
 
--- PowerfulRatesAPI/PowerfulRatesAPI/Program.cs
+++ PowerfulRatesAPI/PowerfulRatesAPI/Program.cs
@@ -3,20 +3,28 @@ using System.Threading;
 using System.Threading.Tasks;
 using EventContracts;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace PowerfulRatesAPI
 {
     class Program
     {
-        public static async Task Main()
-        {      
-            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg => cfg.Host("localhost", hst =>
+        public static IConfiguration Configuration { get; set; }
+        public static async Task Main(string[] args)
+        {
+            using Microsoft.Extensions.Hosting.IHost host = CreateHostBuilder(args).Build();
+            var builder = new ConfigurationBuilder()
+               .AddEnvironmentVariables()
+               .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg => cfg.Host(Configuration.GetSection("Host").Value, hst =>
             {
-                hst.Username("guest");
-                hst.Password("guest");
+                hst.Username(Configuration.GetSection("Login").Value);
+                hst.Password(Configuration.GetSection("Password").Value);
             }));
             
-
             await busControl.StartAsync();
             try
             {
@@ -24,8 +32,9 @@ namespace PowerfulRatesAPI
                 {
                     string value = await Task.Run(() =>
                     {
+                        var currencyRates = CurrencyRates.GetCurrencyRates();
                         Console.WriteLine($"I already sent this shit in {DateTime.Now} you jerk!");
-                        return CurrencyRates.GetCurrencyRates();
+                        return currencyRates;
                     });
 
                     await busControl.Publish<ValueEntered>(new
@@ -39,6 +48,9 @@ namespace PowerfulRatesAPI
             {
                 await busControl.StopAsync();
             }
+            await host.RunAsync();
         }
-    }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args);
+    } 
 }
